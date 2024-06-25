832c1181437e42b8d8110fd48cb003688599fc73
kmccoy3 <kevinmccoy678@gmail.com>
2024-06-24 22:22:04 -0500

    Add time calculator

---
+++ calc_times.sh
@@ -0,0 +1,5 @@
+time {
+python -u "/Users/kmm12/Documents/documents-main/personal/coding/advent_of_code/2023/day_1/day_1.py"
+python -u "/Users/kmm12/Documents/documents-main/personal/coding/advent_of_code/2023/day_2/day_2.py";
+python -u "/Users/kmm12/Documents/documents-main/personal/coding/advent_of_code/2023/day_3/day_3.py";
+}
