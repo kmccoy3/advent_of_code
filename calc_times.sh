e15333c14186154b19b99af99e0a7884faa3ec62
kmccoy3 <kevinmccoy678@gmail.com>
2024-06-25 00:25:39 -0500

    break command over multiple lines

--- calc_times.sh
+++ calc_times.sh
@@ -2,4 +2,8 @@
 
 cd /Users/kmm12/Documents/documents-main/personal/coding/advent_of_code;
 
-hyperfine 'python3 ./2023/day_1/day_1.py' 'python3 ./2023/day_2/day_2.py' 'python3 ./2023/day_3/day_3.py' 'python3 ./2023/day_4/day_4.py' --export-markdown times.md --warmup 3
+hyperfine 'python3 ./2023/day_1/day_1.py' \
+'python3 ./2023/day_2/day_2.py' \
+'python3 ./2023/day_3/day_3.py' \
+'python3 ./2023/day_4/day_4.py' \
+--export-markdown times.md --warmup 3;
