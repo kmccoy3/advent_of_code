1d047ace6e8dd682222f5ea422c003e7dbfdd4ea
kmccoy3 <kevinmccoy678@gmail.com>
2024-06-25 00:18:31 -0500

    add auto CLI timer

--- calc_times.sh
+++ calc_times.sh
@@ -1,5 +1,5 @@
-time {
-python -u "/Users/kmm12/Documents/documents-main/personal/coding/advent_of_code/2023/day_1/day_1.py";
-python -u "/Users/kmm12/Documents/documents-main/personal/coding/advent_of_code/2023/day_2/day_2.py";
-python -u "/Users/kmm12/Documents/documents-main/personal/coding/advent_of_code/2023/day_3/day_3.py";
-}
+#!/bin/bash
+
+cd /Users/kmm12/Documents/documents-main/personal/coding/advent_of_code;
+
+hyperfine 'python3 ./2023/day_1/day_1.py' 'python3 ./2023/day_2/day_2.py' 'python3 ./2023/day_3/day_3.py' 'python3 ./2023/day_4/day_4.py' --export-markdown times.md --warmup 3
